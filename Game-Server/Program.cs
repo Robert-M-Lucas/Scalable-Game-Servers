86c15148c65bcfa7c8d1392b9cb6ae1f174562dc
Robert-App-Dev <robert01.appdev@gmail.com>
2022-05-02 09:53:47 +0100

    commit

--- Game-Server/Program.cs
+++ Game-Server/Program.cs
@@ -1,8 +1,23 @@
-namespace GameServer;
+namespace GameServer;
+
 using System;
+using System.Text.Json;
  
+class Pet
+{
+    public string? Type { get; set; }
+    public string? Name { get; set; }
+    public double Age { get; set; }
+}
+
 public static class Program{
     public static void Main(string[] args){
-        
+        var pets = new List<Pet>
+        {
+            new Pet { Type = "Cat", Name = "MooMoo", Age = 3.4 },
+            new Pet { Type = "Squirrel", Name = "Sandy", Age = 7 }
+        };
+
+        Console.WriteLine(JsonSerializer.Serialize(pets));
     }
 }
