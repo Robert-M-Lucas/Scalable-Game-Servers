a2603103020b74cf847b0bdfe68c8202d39ba489
Robert-App-Dev <robert01.appdev@gmail.com>
2022-05-07 15:51:36 +0100

    commit

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
 
-# os.system('.Build\\Server-Spooler\\Server-Spooler.exe "C:\\Users\\rober\Documents\\Mixed Code Projects\\Scalable Game Servers\\config.json"')
+os.system('.Build\\Server-Spooler\\Server-Spooler.exe "C:\\Users\\rober\Documents\\Mixed Code Projects\\Scalable-Game-Servers\\config.json"')
+# C:\Users\rober\Documents\Mixed Code Projects\Scalable-Game-Servers\config.json
 
-os.system('.Build\\Server-Spooler\\Server-Spooler.exe "C:\\Users\\rober\\Documents\\Projects\\Scalable-Game-Servers\\config.json"')
+# os.system('.Build\\Server-Spooler\\Server-Spooler.exe "C:\\Users\\rober\\Documents\\Projects\\Scalable-Game-Servers\\config.json"')
